1d1279d93233e592154e613c81e216e53f1a4dfd
baskluiters <bas@kluiters.com>
2024-10-30 06:58:08 +0100

    Moved battery level input to ADC1_CHANNEL_2

--- src/adc.cpp
+++ src/adc.cpp
@@ -3,15 +3,12 @@
 #include "config.h"
 #include "adc.h"
 
-#define CHAN_VBAT    ADC1_CHANNEL_0 //((adc1_channel_t)0)
+#define CHAN_VBAT    ADC1_CHANNEL_2
 // two-point calibration for slope & intercept using ATTEN_DB_0, 10bit resolution
 #define V1		3.252f
 #define ADC1	723.0f
 #define V2		4.080f
 #define ADC2	908.0f
-
-static const float battery_max = 4.20; //maximum voltage of battery
-static const float battery_min = 3.0;  //minimum voltage of battery before shutdown
 
 static float BatteryVoltage;
 
@@ -28,7 +25,7 @@ static void adc_update_average_battery_voltage(unsigned nr_of_samples) {
 void adc_init() {
     adc1_config_width(ADC_WIDTH_BIT_12); // no 10-bit resolution option !?
 	// need to ensure maximum voltage at adc pin is < 800mV for ATTEN_DB_0
-    adc1_config_channel_atten(CHAN_VBAT, ADC_ATTEN_DB_0);
+    adc1_config_channel_atten(CHAN_VBAT, ADC_ATTEN_DB_11);
 	adc_update_average_battery_voltage(4);
 }
 
@@ -43,9 +40,10 @@ float adc_get_battery_voltage(void) {
 }
 
 float adc_get_battery_percentage(void) {
-	float percentage = ((BatteryVoltage - battery_min) / (battery_max - battery_min)) * 100;
-    if (percentage < 100)
-        return percentage;
-    else
+	float percentage = ((adc_get_battery_voltage() - V1) / (V2 - V1)) * 100.0f;
+    if( percentage > 100.0f )
         return 100.0f;
+    if( percentage < 0.0f )
+        return 0.0f;
+	return percentage;
 }
